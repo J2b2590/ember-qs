191048f009026e19659a934c6fa3cc7727032dfc
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2018-02-12 13:35:54 -0600

    api wont stop

--- app/components/fetch-chuck.js
+++ app/components/fetch-chuck.js
@@ -4,12 +4,16 @@ import Component from '@ember/component';
 
 
 export default Component.extend({
+
 	willRender() {
 		$.getJSON('https://api.chucknorris.io/jokes/random').then(data =>{
 			console.log(data.value)
+
 			this.set('chuck', data.value);
 			
-
 		});
 	}
+
+	
+
  });
