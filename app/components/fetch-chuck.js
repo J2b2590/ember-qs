c303c4f9645a5ef0956ff16e81783a07be037f15
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2018-02-19 14:15:16 -0600

    figured out how to get one joke at a time

--- app/components/fetch-chuck.js
+++ app/components/fetch-chuck.js
@@ -4,13 +4,28 @@ import Component from '@ember/component';
 
 
 export default Component.extend({
+	
+	gotJoke: false,
+	joke: '',
+	actions:{
+		getJoke: function(){
+			var that = this;
+			$.ajax({
+				type: 'GET',
+				url: 'https://api.chucknorris.io/jokes/random',
+				success: function(response){
+					console.log(response.value)
+					that.setProperties({
+						joke: response.value,
+						gotJoke: true
+					});
 
-	willRender() {
-		$.getJSON('https://api.chucknorris.io/jokes/random').then(data =>{
-			console.log(data.id)
-
-			this.set('chuck', data.value);
-			
-		});
+				},
+				error: function() { 
+					alert("Jokes on you");
+				}
+			});
+		}
 	}
- });
+
+});
